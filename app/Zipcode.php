5ef3e093ed56d1298cb76e114517a171547d643e
Clinton Andrews <candrews800@gmail.com>
2015-06-02 00:14:50 -0400

    added getNearbyZipcodes

--- app/Zipcode.php
+++ app/Zipcode.php
@@ -1,8 +1,7 @@
-<?php
-
-namespace App;
+<?php namespace App;
 
 use \Illuminate\Database\Eloquent\Model;
+use \Illuminate\Support\Facades\DB;
 
 class Zipcode extends Model{
 
@@ -19,5 +18,20 @@ class Zipcode extends Model{
         ];
 
         return self::create($fields);
+    }
+
+    public function getNearbyZipcodes($distance){
+        return self::nearby($this->lat, $this->lon, $distance)->distinct()->lists('zipcode');
+    }
+
+    public function scopeNearby($query, $lat, $lon, $distance){
+        return $query->select(
+            DB::raw('( 3959 * acos( cos( radians('.$lat.') ) * cos( radians( lat ) )
+                                            * cos( radians(lon) - radians('.$lon.')) + sin(radians('.$lat.'))
+                                            * sin( radians(lat)))) AS distance, zipcode '
+            ))
+            ->having('distance', '<', $distance);
+
+
     }
 }
